5d6deb4812701192ec0c7a0e548ce31ddedb5cb9
Fahima BOUMELLIL <fboumell@e1r2p6.42.fr>
2021-11-25 15:32:57 +0100

    a

--- server.c
+++ server.c
@@ -6,11 +6,13 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 11:08:50 by fboumell          #+#    #+#             */
-/*   Updated: 2021/11/24 17:52:37 by fboumell         ###   ########.fr       */
+/*   Updated: 2021/11/25 15:27:17 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static int len;
 
 void	get_my_pid(void)
 {
@@ -22,24 +24,40 @@ void	get_my_pid(void)
 	ft_putchar('\n');
 }
 
-void	handler(int signum)
+void	convert_bits(char *s)
 {
 	char	c;
-	int		i;
+	int	i;
 
 	c = 0;
-	i = 0;
-	if (signum == SIGUSR1)
-		c |= 1 << i;
-	i++;
-	if (i == 8)
+	i = 7;
+	while (i >= 0)
 	{
-		if (c == '\0')
-			ft_putchar('\n');
-		else
-			ft_putchar(c);
-		c = 0;
-		i = 0;
+		if (s[i] == '1')
+			c |= 1 << (7 - i);
+		i--;
+	}
+	free(s);
+	ft_putchar(c);
+	
+}
+
+void	handler(int signum)
+{
+	char *s;
+	
+	if (len == 0)
+		s = malloc(sizeof(char) * (8 + 1));
+	if (signum == SIGUSR1)
+		s[len] = '1';
+	if (signum == SIGUSR2)
+		s[len] = '0';
+	len++;
+	if (len == 8)
+	{
+		s[len] = '\0';
+		convert_bits(s);
+		len = 0;
 	}
 }
 
@@ -47,9 +65,10 @@ int	main()
 {
 	struct sigaction	sa;
 	
-	get_my_pid();
+	len = 0;
 	sa.sa_handler = handler;
 	sa.sa_flags = 0;
+	get_my_pid();
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
